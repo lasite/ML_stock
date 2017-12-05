7a92cdc2d6c535a5705a35199500ab1078c14681
Tan Rui <tanrui@smartisan.com>
2017-12-05 14:26:56 +0800

    INIT version

---
+++ data_collecter_sohu.py
@@ -0,0 +1,41 @@
+import numpy as np
+import json
+import urllib.request as Request
+import pandas as pd
+url_templete='http://q.stock.sohu.com/hisHq?code=%s_%s&start=%s&end=%s&stat=1&order=D&period=d&callback=historySearchHandler&rt=jsonp'
+cols=['date','open','close','price_change','p_change_rate','low','high','volume','amount','turnover']
+
+def spell_url(domain, code,start,end):
+    url=url_templete%(domain,code,start,end)
+    return url
+
+def get_market_data(code,start,end,is_index=False):
+    if not is_index:
+        domain='cn'
+    else:
+        domain='zs'
+    url = spell_url(domain,code,start,end)
+    request = Request.Request(url)
+    response = Request.urlopen(request)
+    lines = response.read()
+    lines = lines.decode('gb2312')
+    js = json.loads(lines[22:-3])
+    df = pd.DataFrame(js['hq'], columns=cols)
+    df = df.applymap(lambda x:x.strip('%'))
+    df = df.applymap(lambda x: x.replace(u'-', u''))
+    df[df==''] = 0
+    for col in cols[1:]:
+        df[col]=df[col].astype(float)
+    df = df.set_index('date')
+    df = df.sort_index(ascending = False)
+    return df
+
+def get_hist_data(code,start,end,with_index=True):
+    if with_index:
+        df = get_market_data(code,start,end,False)
+        df_index = get_market_data('399001', start,end,True)
+        for col in cols[1:-1]:
+            col_ = 'index_' + col
+            df[col_]=df_index[col]
+        return df
+    return get_market_data(code,start,end,False)
